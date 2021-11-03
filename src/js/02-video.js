67ac272d10dbd258fcb1eba1e18b2d790e1c4b09
Korotkova-Iuliia <3589900@gmail.com>
2021-11-03 16:43:58 +0200

    task 02 corrected

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,32 +4,21 @@ const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 console.log(player);
 
-let currentTimeStopPlayer = localStorage.getItem('videoplayer-current-time = data.seconds');
+let currentTimeStopPlayer = localStorage.getItem('videoplayer-current-time');
 console.log(currentTimeStopPlayer);
 
-// const onPause = function (data) {
-//   //   const timeStopPlayer = data.seconds;
-//   localStorage.setItem('videoplayer-current-time = data.seconds', timeStopPlayer);
-//   //   console.log(timeStopPlayer);
-// };
-// const onPlay = function (data) {
-//   timeOnPlayer = data.seconds;
-//   console.log(timeOnPlayer);
-// };
-const currentTime = player.on(
+const currentTimePlayer = player.on(
   'timeupdate',
   throttle(function (data) {
-    const timeStopPlayer = data.seconds;
-    localStorage.setItem('videoplayer-current-time = data.seconds', data.seconds);
-    console.log(timeStopPlayer);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    console.log(data.seconds);
   }, 1000),
 );
 
 player
   .setCurrentTime(currentTimeStopPlayer)
   .then(function (seconds) {
-    // player.on('pause', onPlaybackPause);
-    player.on('timeupdate', throttle(currentTime, 3000));
+    currentTimePlayer;
   })
   .catch(function (error) {
     switch (error.name) {
@@ -42,42 +31,3 @@ player
         break;
     }
   });
-// //////////////////////////////////////////////////////////////////////////////
-// let currentTimeStopPlayer = localStorage.getItem('videoplayer-current-time = data.seconds');
-// console.log(currentTimeStopPlayer);
-
-// const onPlaybackPause = function (data) {
-//   const timeStopPlayer = data.seconds;
-//   localStorage.setItem('videoplayer-current-time = data.seconds', timeStopPlayer);
-//   console.log(data.seconds);
-// };
-// // const onPlaybackPlay = function (data) {
-// //   localStorage.setItem('videoplayer-current-time = data.seconds', data.seconds);
-// //   player.on('timeupdate', data.seconds);
-// // };
-
-// // player
-// //   .setCurrentTime(currentTimeStopPlayer)
-// //   .then(function (seconds) {
-// //     player.on('pause', onPlaybackPause);
-// //     // player.on('timeupdate', throttle(onPlaybackPlay, 1000));
-// //   })
-// //   .catch(function (error) {
-// //     switch (error.name) {
-// //       case 'RangeError':
-// //         // the time was less than 0 or greater than the video’s duration
-// //         break;
-
-// //       default:
-// //         // some other error occurred
-// //         break;
-// //     }
-// //   });
-
-// // const onPlaybackPlay = function (data) {
-// //   localStorage.setItem('videoplayer-current-time = data.seconds', data.seconds);
-// //   console.log('videoplayer-current-time');
-// // };
-// // // const throttled = throttle(onPlaybackPlay, 1000);
-
-// // // Player.on('timeupdate', throttled);
