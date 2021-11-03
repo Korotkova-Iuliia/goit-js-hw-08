8052f3261c32b5a24b2ea26eb042ca76e4199f5b
Korotkova-Iuliia <3589900@gmail.com>
2021-11-03 20:20:27 +0200

    task 03 throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,37 @@
+const throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
 
 let inputData = {};
 
 initForm();
 
-form.addEventListener('input', e => {
-  inputData[e.target.name] = e.target.value;
-  //   console.log(e.target.name);
-  //   console.log(e.target.value);
-  //   console.log(inputData);
-  localStorage.setItem('inputData', JSON.stringify(inputData));
-});
+form.addEventListener(
+  'input',
+  throttle(e => {
+    inputData[e.target.name] = e.target.value;
+    localStorage.setItem('inputData', JSON.stringify(inputData));
+  }, 500),
+);
 
 form.addEventListener('submit', e => {
   e.preventDefault();
 
   const formData = new FormData(form);
-
   formData.forEach((email, message) => console.log(email, message));
+  console.log(JSON.parse(localStorage.getItem('inputData')));
   form.reset();
   localStorage.removeItem('inputData');
 });
 
 function initForm() {
   let inputForm = localStorage.getItem('inputData');
-  console.log(inputForm);
 
   if (inputForm) {
     inputForm = JSON.parse(inputForm);
-    console.log(inputForm);
+
     Object.entries(inputForm).forEach(([name, value]) => {
       inputData[name] = value;
       form.elements[name].value = value;
     });
   }
-  //
 }
